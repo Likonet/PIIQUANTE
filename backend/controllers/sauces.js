f8568bbd2b4213532b06ba772cbef73f78b29e55
Likonet <dyplomats@hotmail.com>
2022-06-22 20:19:18 -0300

    like et dislike
    like et dislike

--- backend/controllers/sauces.js
+++ backend/controllers/sauces.js
@@ -14,32 +14,53 @@ exports.createSauce = (req, res, next) => {
       .catch(error => res.status(400).json({ error }));
   };
 
-  exports.feedBackSauce = (req, res, next) => { 
-  console.log(req.body)
+  exports.feedBackSauce = async (req, res, next) => { 
+  try {
+  const idSauce =  await Sauces.findOne ({ _id: req.params.id }); // pourquoi await?
+  
+  switch (req.body.like)
+    {
+    case 1:
+      const filter = { 
+        likes: idSauce.likes + 1,
+        usersLiked: [...idSauce.usersLiked, req.body.userId]
+      }
+      const upLike = await idSauce.updateOne(filter);
+      console.log(upLike);
+      break;
+
+    case -1:
+     const filter2 = {
+      dislikes: idSauce.dislikes + 1,
+      usersDisliked: [...idSauce.usersDisliked, req.body.userId]
+     }
+     console.log(filter2)
+   const downLike = await idSauce.update(filter2);
+   break;
+   default:
+    }   
+  }
+catch(error){
+  return res.status(500).json({error: error.message})
+}
+return res.status(203).json({message: "sauce à jour"})
+  };
 
   // Clique sur l'un des pouces
   //envoi requête POST qui contient le userId et le like à 1 ou -1
-  // Si likes =1 ou -1, mettre à jour la sauce de la base de donnée 
+  // Si likes =1 ou -1, mettre à jour la sauce dans la base de donnée 
   // avec like qui passe de 0 à 1
   // Mettre le userId de cette sauce dans le tableau usersLiked / userDisliked.
 
+ 
+  
 
-  //Sauces.find({usersLiked:req.body.userId})
- /* let test = new Sauces(
-  {
-    likes : req.body.likes,
-  })
-  test.save()*/
-  Sauces.updateOne({ userId: req.body.userId }, { ...req.body, userId: req.body.userId })
-  .then(() => res.status(200).json({ message: 'Objet modifié !'}))
-  .catch(error => res.status(400).json({ error }));
-
-  };
+  
 
 
 exports.getAllSauce = (req, res, next) => { // fonction qui est appelé à une requête de l'appli // api/stuff = l'url qui est visé par l'appli front end, un tableau
  
- console.log(Sauces.find());
+// console.log(Sauces.find());
 
  Sauces.find() //Lit dans la base les sauces    // methode find mise à dispo par le modèle
     
@@ -48,7 +69,6 @@ exports.getAllSauce = (req, res, next) => { // fonction qui est appelé à une r
 };
 
 exports.getOneSauce = (req, res, next) => {
-    console.log('-----------')
     Sauces.findOne ({ _id: req.params.id })  // req.params.id  pour obtenir l'id grâce à l'objet params.id car c'est un paramètre de route dynamique
     .then(sauces => res.status(200).json(sauces))
     .catch(error => res.status(404).json({ error }));
